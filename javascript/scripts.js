d71b869f8bedd49933cc057e96f5b89ad41bfce4
Philip Winchester <philipwinc@gmail.com>
2016-10-09 12:01:59 -0400

    added straw death

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,45 +1,52 @@
-console.log("JAVASCRIPT LOADED");
+console.log("SCRIPTS LOADED");
 
 $(function(){
   console.log("DOM LOADED");
   var enemyObjList = [];
+  var unitObjList = [];
+  var timerId;
 
   var grid = new Grid();
   var gridObj = grid.getGrid();
 
   var castle = new Castle();
-  $("body").append(castle);
+  var castleDom = castle.getCastle();
+  $("body").append(castleDom);
 
   $(".menu").draggable({
     helper: "clone",
     grid: [5,5],
-    // grid: [Math.floor($(window).width() / 15), Math.floor($(window).height() / 10)],
     start: function(){
-      // console.log(String($(this).attr("id")));
       var elemClass = String($(this).attr("id")).split("_")[1];
 
       $(".menu").eq($(".menu").length - 1).addClass(elemClass);
       $(".menu").eq($(".menu").length - 1).removeClass("menu");
-      // $(".menu").eq($(".menu").length - 1).removeAttr("menu_"+elemClass);
-    },
-    end: function(){
-      console.log($(this).data());
     }
   });
+
   $(".box").droppable({
     drop: function(e,ui){
-      var newUnit = $(ui.helper).clone();
+      var newDom = $(ui.helper).clone();
+      newDom.removeClass("ui-draggable ui-draggable-dragging ui-draggable-handle");
 
-      newUnit.removeClass("ui-draggable ui-draggable-dragging ui-draggable-handle");
+      if (newDom.attr("class") === "straw") {
+        console.log("straw");
+        var newUnit = new Straw();
+      } else if (newDom.attr("class") === "match") {
+        console.log("match");
+        var newUnit = new Match();
+      }
 
       var colNum = $(this).data().colNum;
       var rowNum = $(this).data().rowNum;
+      newUnit.setCoords(colNum,rowNum);
+      console.log(newUnit.getCoords());
 
       var boxCoords = grid.getCenter(colNum, rowNum)
+      newDom.css({"left":Math.floor(boxCoords[0])+"px", "top":Math.floor(boxCoords[1])+"px"})
+      gridObj[colNum][rowNum].append(newDom);
 
-      newUnit.css({"left":Math.floor(boxCoords[0])+"px", "top":Math.floor(boxCoords[1])+"px"})
-
-      $(".grid").append(newUnit);
+      unitObjList.push(newUnit);
     }
   });
 
@@ -49,50 +56,39 @@ $(function(){
 
     var yCoord = Math.floor(Math.random() * gridObj[0].length);
     eraseObj.setCoords(0,yCoord);
-    var boxCoords = grid.getCenter(0,yCoord);
 
+    var boxCoords = grid.getCenter(0,yCoord);
     eraseDom.css({"left":Math.floor(boxCoords[0])+"px", "top":Math.floor(boxCoords[1])+"px"});
+    gridObj[0][yCoord].append(eraseDom);
 
     enemyObjList.push(eraseObj);
-    $(".grid").append(eraseDom);
   };
-  window.setInterval(function() {
-    for (var i = 0; i < enemyObjList.length; i++) {
-      // enemyObjList[i].march();
+
+  //timer start stop for debugging purposes
+  var startTimer = function(event) {
+    if (timerId) {
+      return;
     }
-  }, 1000)
-});
-
-/*spawn units on row 15 column 10 and move them up if overlap*/
-
-// window.setInterval(function() {
-//     $('#result').text(collision($('#base'), $('#div2')));
-// }, 200);
-
-function collision($unit1, $unit2) {
-  var left1 = $unit1.offset().left;
-  var top1 = $unit1.offset().top;
-  var height1 = $unit1.outerHeight(true);
-  var width1 = $unit1.outerWidth(true);
-  var bottom1 = top1 + height1;
-  var right1 = left1 + width1;
-
-  var left2 = $unit2.offset().left;
-  var top2 = $unit2.offset().top;
-  var height2 = $unit2.outerHeight(true);
-  var width2 = $unit2.outerWidth(true);
-  var bottom2 = top2 + height2;
-  var right2 = left2 + width2;
-
-  if (bottom1 < top2 || top1 > bottom2 ||
-     right1 < left2 || left1 > right2) {
-    console.log($unit1.attr("id"));
-    return false;
-  } else {
-    console.log("COLLISION");
-    return true;
+    timerId = setInterval(function(){
+      console.log("starting timer");
+      for (var i = 0; i < enemyObjList.length; i++) {
+        enemyObjList[i].march();
+      }
+    }, 1000);
   }
-}
+  var stopTimer = function(event) {
+    clearInterval(timerId);
+    timerId = null;
+    console.log('stop Timer');
+  }
+  $("#castle").on("click",function(event) {
+    if (timerId) {
+      stopTimer();
+    } else {
+      startTimer();
+    }
+  });
+});
 
 /*~~~~~~~~~~~~~CLASSES~~~~~~~~~~~~~*/
 class Grid {
@@ -144,6 +140,9 @@ class Unit {
   getCoords(){
     return [this.x, this.y];
   }
+  getNeighbors(){
+    return []
+  }
 }
 class Eraserman extends Unit {
   constructor(grid){
@@ -152,16 +151,22 @@ class Eraserman extends Unit {
 
     this.unit = $("<div>");
     this.unit.attr("class", "erase");
-
-    var boxCoords = this.grid.getCenter(this.x,this.y);
-    this.unit.css({"left":Math.floor(boxCoords[0])+"px",
-      "top":Math.floor(boxCoords[1])+"px"
-    });
   }
   getUnit(){
     return this.unit;
   }
   march(){
+    let gridList = this.grid.getGrid();
+    // console.log("Column: " + (this.x + 2) + " Row: " + this.y);
+    // gridList[this.x + 2][this.y].css("background-color","rgba(255,0,0,.5)");
+    // console.log(gridList[this.x + 2][this.y].children().attr("class"));
+
+    if (gridList[this.x + 1][this.y].children().attr("class") === "straw" ||
+      gridList[this.x + 1][this.y].children().attr("class") === "match") {
+      console.log("WATCH OUT");
+      // console.log("Row: " + (this.x + 1) + " : " + "Column: " + this.y);
+      gridList[this.x + 1][this.y].empty();
+    }
     this.x += 1;
 
     var boxCoords = this.grid.getCenter(this.x,this.y);
@@ -171,43 +176,23 @@ class Eraserman extends Unit {
 class Match extends Unit {
   constructor(){
     super();
-
-    /*
-    1 - spawns absolute fixed
-    2 - on click becomes draggable
-    3 - on release snaps to grid
-    */
-
-    this.unit = $("<div>")
-    this.unit.attr("class", "match")
-    this.unit.css({"left": $(window).width() * .2, "top": $(window).height() * .4});
-    // this.unit.offset({"left": "20vw", "top": "40vh"});
-
-    this.unit.draggable();
-
-    return this.unit;
   }
 }
 class Straw extends Unit {
   constructor() {
     super();
-
-    this.unit = $("<div>")
-    this.unit.attr("class", "straw")
-    this.unit.offset({"left": $(window).width() * .3, "top": $(window).height() * .4});
-    // this.unit.offset({"left": "30vw", "top": "40vh"});
-
-    return this.unit;
   }
 }
 class Castle {
   constructor() {
-    // console.log("made Castle");
+    this.health = 10;
 
     this.unit = $("<div>")
     this.unit.attr("id", "castle")
     this.unit.offset({"left": "85vw", "top": "40vh"});
-
+    this.unit.text("Health: " + 10)
+  }
+  getCastle(){
     return this.unit;
   }
 }
